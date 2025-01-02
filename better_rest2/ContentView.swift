0c751369001e7dfa15f097e74cfd30a1f6198d25
CharlesMich <charleedongo@hotmail.com>
2025-01-02 12:41:17 -0600

    Initial Commit

---
+++ better_rest2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  better_rest2
+//
+//  Created by Charles Michael on 1/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ better_rest2/better_rest2App.swift
@@ -0,0 +1,17 @@
+//
+//  better_rest2App.swift
+//  better_rest2
+//
+//  Created by Charles Michael on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct better_rest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
